4a16bd97e138d37c47f97edeb8f3a98ffdd66984
LuizGarbini <garbasneto@gmail.com>
2023-03-14 13:19:22 -0300

    :wrench: styles updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import { useState } from "react";
 import "./App.css";
 import img from "./logo.svg";
+import { PlusCircle } from "@phosphor-icons/react";
 
 import "./global.css";
 
 export function App() {
   return (
     <div className="App">
-      <div>
+      <header>
         <img src="../public/logo.svg" alt="" />
-      </div>
-      <div className="InputBackground">
-        <input type="text" />
-        <button> Criar </button>
+      </header>
+      <div className="InputAndButton">
+        <div className="InputSize">
+          <input type="text" placeholder="Adicione um nova tarefa" />
+          <button>
+            {" "}
+            Criar <PlusCircle size={22} />{" "}
+          </button>
+        </div>
       </div>
     </div>
   );
