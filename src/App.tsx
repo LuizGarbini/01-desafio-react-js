9513ca756333c8dc6bb679851fe250125fe544a8
LuizGarbini <garbasneto@gmail.com>
2023-03-22 23:14:03 -0300

    :wrench: tasks functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import img from "./logo.svg";
 import { PlusCircle } from "@phosphor-icons/react";
 import { Tasks } from "./components/Tasks";
-import { Comment } from "./components/Comment";
+import { Task } from "./components/Task";
 
 import "./global.css";
 import stylesTasks from "./components/Tasks.module.css";
@@ -10,7 +10,50 @@ import stylesApp from "./App.module.css";
 
 const comments = [1, 2, 3];
 
+export type Task = {
+  id: number;
+  title: string;
+  isCompleted: boolean;
+};
+
+type Tasks = Array<Task>;
+
 export function App() {
+  const [tasks, setTasks] = useState<Tasks | null>(null);
+  const [newTaskTitle, setNewTaskTitle] = useState("");
+
+  function handleAddTask() {
+    if (!newTaskTitle) return;
+
+    const newTask = {
+      id: Date.now(),
+      title: newTaskTitle,
+      isCompleted: false,
+    };
+
+    setTasks(tasks ? [...tasks, newTask] : [newTask]);
+    // setNewTaskTitle("");
+  }
+
+  function handleToggleTaskStatus(id: number) {
+    setTasks((prevTasks) =>
+      prevTasks!.map((task) => {
+        return {
+          ...task,
+          isCompleted: task.id === id ? !task.isCompleted : task.isCompleted,
+        };
+      })
+    );
+  }
+
+  function handleRemoveTask(id: number) {
+    setTasks((prevTasks) => prevTasks!.filter((task) => task.id !== id));
+  }
+
+  useEffect(() => {
+    console.log({ tasks });
+  }, [tasks]);
+
   return (
     <div>
       <header className={stylesApp.header}>
@@ -18,8 +61,13 @@ export function App() {
       </header>
       <div className={stylesApp.inputAndButton}>
         <div className={stylesApp.inputSize}>
-          <input type="text" placeholder="Adicione um nova tarefa" />
-          <button>
+          <input
+            onChange={(event) => setNewTaskTitle(event.target.value)}
+            value={newTaskTitle}
+            type="text"
+            placeholder="Adicione uma nova tarefa"
+          />
+          <button onClick={handleAddTask}>
             {" "}
             Criar <PlusCircle size={22} />{" "}
           </button>
@@ -29,8 +77,15 @@ export function App() {
         <Tasks />
       </div>
       <div>
-        {comments.map((comment) => {
-          return <Comment />;
+        {tasks?.map((task) => {
+          return (
+            <Task
+              task={task}
+              handleToggleTaskStatus={handleToggleTaskStatus}
+              handleRemoveTask={handleRemoveTask}
+              key={task.id}
+            />
+          );
         })}
       </div>
     </div>
---
+++ src/components/Task.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import stylesTask from "./Task.module.css";
+import { Task as TaskType } from "../App";
+
+type TaskProps = {
+  task: TaskType;
+  handleToggleTaskStatus: (id: number) => void;
+  handleRemoveTask: (id: number) => void;
+};
+
+export function Task(props: TaskProps) {
+  const {
+    task: { title, id, isCompleted },
+    handleToggleTaskStatus,
+    handleRemoveTask,
+  } = props;
+
+  return (
+    <div className={stylesTask.Content}>
+      <div
+        className={
+          isCompleted ? stylesTask.ContentDivCompleted : stylesTask.ContentDiv
+        }
+      >
+        <input
+          type="checkbox"
+          name="radio"
+          onChange={() => handleToggleTaskStatus(id)}
+        />
+
+        <p>{title}</p>
+      </div>
+
+      <button onClick={() => handleRemoveTask(id)}>excluir</button>
+    </div>
+  );
+}
